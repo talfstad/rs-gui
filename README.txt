b9d75eb33561cb1c7cf8557aa707281feda85fe0
Buildcave <trevor@buildcave.com>
2014-10-26 13:18:54 -0700

    - Updated the obfuscation process to use a 2 stage process not dependent on jquery. - Updated all calls to never expose a user email in front end code by adding a secret string in the database. - Added a STORED_PROCEDURES.sql to track stored procedures - Added a few npm packages

--- README.txt
+++ README.txt
@@ -1,4 +1,4 @@
-To get started with front end development:
+To get started with FULL SERVER development:
 
 1. Install Sublime Text Editor, set tab size to 2 and convert tab to spacing
 
@@ -18,10 +18,22 @@ NOTE: These commands will install the appropriate libraries to run the code.
 
 #### Running the app
 
+Make sure you are using `localhost` as the compressed-landercode-local.js and compressed-initial.js require it.
+If you are using some other domain you will need to regenerate these files.
+
+
 Run Distribution version:
   `sudo grunt build`
   `sudo grunt serve`
 
-
   you will now have a server running on port 9000 that is connected to our db 
   if it launches as a 404 just refresh the page
+
+
+
+###DEPLOYMENT####
+
+Whenever you deploy to a non localhost server you will need to:
+
+1. update all client files to reflect the server domain name
+2. regenerate the compressed version of the client files
--- app.js
+++ app.js
@@ -6,8 +6,6 @@ var express = require('express')
   , path = require('path');
 var app = express();
 
-//var $ = require('jquery');
-
 // all environments
 app.set('port', process.env.PORT || 9000);
 app.set('views', __dirname + '/views');
@@ -26,39 +24,44 @@ app.configure('development', function() {
     app.locals.pretty = true;
 });
 
+var compressor = require('yuicompressor');
+var validator = require('validator');
+var bcrypt = require('bcrypt-nodejs');
+
+
+
 app.get('/', checkAuth, function( req, res) {
 	res.render('index');
 });
 
+var randomstring = require("randomstring");
 var obf = require('node-obf');
-var UglifyJS = require("uglify-js");
 var fs = require("node-fs");
+
 app.get('/landercode', checkAuth, function( req, res) {
+    var user = req.session.user_id; 
+    fs.readFile('./client/compressed/compressed-initial.js', function(err, data) {
+        if(err) throw err;
+        connection.query("SELECT secret_username FROM users WHERE user = ?", [user], function(err, docs) {
+            if(docs.length == 1) {
+                var replacedFile = String(data).replace('replaceme', docs[0].secret_username);       
 
-	var user = req.session.user_id;  
-	
-	fs.readFile('public/resources/clickjacker-min.txt', function(err, data) {
-		if(err) throw err;
-		var str = String(user).split("@");
-		var first = str[0];
-		var str2 = String(str[1]).split(".");
-		second = str2[0];	
-		
-		var replacedFile = String(data).replace('%first%', first);		
-		replacedFile = String(replacedFile).replace('%second%', second);		
-
-	//	var result = UglifyJS.minify(String(replacedFile), {fromString: true});
-		
-		res.send({
-			landercode : replacedFile
-		});	
-
-
-	});	
-	
+                compressor.compress(replacedFile, {
+                    //Compressor Options:
+                    charset: 'utf8',
+                    type: 'js',
+                    nomunge: true,
+                    'line-break': 80
+                }, function(err, data, extra) {
+                    res.send({
+                        landercode:  String(replacedFile)
+                    });
+                }); 
+            }
+        });
+    });     
 });
 
-//connect to mysql database
 var connection = mysql.createConnection({
 	host : '54.187.151.91',
 	user : 'root',
@@ -67,9 +70,7 @@ var connection = mysql.createConnection({
 });
 connection.connect();
 
-var validator = require('validator');
 
-var bcrypt = require('bcrypt-nodejs');
 
 app.get('/login', function(req, res) {
     res.render('login');
@@ -78,7 +79,6 @@ app.get('/login', function(req, res) {
 function checkAuth(req, res, next) {
     if (!req.session.user_id) {
         res.redirect('login');
-        //res.send('You are not authorized.');
     } else {
         next();
     }
@@ -96,22 +96,12 @@ app.post('/login', function (req, res) {
     var post = req.body;
     var password = post.password;
     var username = post.username;
-    //console.log("username: %s", post.username);
-    //console.log("password: %s", post.password);
-
-    //console.log('here-1');
-
     var hash;
+
     connection.query('SELECT hash FROM users WHERE user = ? && approved = 1;', [username], function(err, rows) {
         if (err) {
             res.json(err);
         }
-        //console.log(rows);
-        //console.log(rows.length);
-        //console.log(rows[0]);
-
-        //console.log('here0');
-
         if(rows.length == 1) {
             hash = rows[0].hash;
         } 
@@ -119,7 +109,6 @@ app.post('/login', function (req, res) {
             var msg = {status: 'User does not exist or is not yet approved.'}
             res.render('login', {data: msg});
         }
-        //console.log(bcrypt.compareSync(password, hash));
 
         bcrypt.compare(password, hash, function(err, response) {
             if(response == true) {
@@ -131,13 +120,14 @@ app.post('/login', function (req, res) {
                 res.render('login', {data: msg});
             }
         });
-    })
+    });
 
 });
 
 app.get('/register', function (req, res) {
     res.render('register');
 }); 
+
 
 app.post('/register', function (req, res) {
     var post = req.body;
@@ -150,7 +140,7 @@ app.post('/register', function (req, res) {
             var msg = {status: 'User already exists.'}
             res.render('register', {data: msg});
         }
-    })
+    });
 
     if (!validator.isEmail(email)) {
         var msg = {status: 'Email address invalid.'}
@@ -165,10 +155,24 @@ app.post('/register', function (req, res) {
         res.render('register', {data: msg});
     }
     else {
+        //adding user!
+        var secret = randomstring.generate(30);
+        var secretNotUnique = true;
+        
+        //ensure unique string
+        connection.query("SELECT secret_username FROM users;", function(err, docs) {
+            for(var i=0 ; i<docs.length ; i++) {
+                if(secret == docs[i]) {
+                    secret = randomstring.generate(30);
+                    i=0; //start over
+                }
+            }
+        });
+    
         bcrypt.hash(password, null, null, function(err, hash) {
-            connection.query('INSERT INTO users (user, hash, approved) VALUES(?, ?, ?);', [email, hash, 0], function(err, docs) {
+            connection.query('INSERT INTO users (user, hash, secret_username, approved) VALUES(?, ?, ?, ?);', [email, hash, secret, 0], function(err, docs) {
                 if (err) res.json(err);
-            })
+            });
         });
 
         var msg = {status: 'Registration submitted and waiting for approval! An email will be sent to upon approval.'}
@@ -182,10 +186,6 @@ app.get('/logout', checkAuth, function (req, res) {
     res.redirect('/login');
 });  
 
-//app.get('/test/:id', add.test);
-
-/******************** my_domains ********************/
-
 app.get('/my_domains', checkAuth, function (req, res) {
     connection.query('select * from my_domains where user = ?', [req.session.user_id],function(err, docs) {
         res.render('my_domains', {domains: docs});
@@ -196,19 +196,16 @@ app.get('/my_domains', checkAuth, function (req, res) {
 // Save the new registered domain
 app.post('/my_domains', checkAuth, function (req, res) {
         var url=req.body.url;
-        console.log("req: %s", req.body.url );
         if(url) {
             connection.query('CALL insert_my_domain(?, ?);', [url, req.session.user_id], function(err, docs) {
                 if (err) res.json(err);
                 res.redirect('my_domains');
             })
         }
-
 });
 
-app.get('/my_domains/delete', checkAuth, function (req, res){
+app.get('/my_domains/delete', checkAuth, function (req, res) {
     var id=req.query.id;
-    console.log("delete req: %s", id);
     
     connection.query('DELETE from my_domains where id = ?;', [id], function(err, docs) {
         if (err) res.json(err);
@@ -217,9 +214,6 @@ app.get('/my_domains/delete', checkAuth, function (req, res){
         }
     })
 });
-
-/******************** all_domains ********************/
-
 
 app.get('/all_domains', checkAuth, function (req, res) {
     connection.query('select url, id, registered, count, rate from all_domains where user = ?', [req.session.user_id], function(err, docs) {
@@ -232,11 +226,7 @@ app.get('/all_domains/list', checkAuth, function(req, res) {
         if (err) res.json(err);
         else {
             res.send({
-//                    result: 'success',
-//                    err:    '',
-//                    fields: fields,
-                    json:   rows
-//                    length: rows.length
+                    json: rows
             });
         }
     });
@@ -244,7 +234,6 @@ app.get('/all_domains/list', checkAuth, function(req, res) {
 
 app.post('/all_domains/delete', checkAuth, function (req, res) {
     var id=req.body.id;
-    console.log("delete req: %s", id);
     
     connection.query('DELETE from all_domains where id = ?;', [id], function(err, docs) {
         if (err) res.json(err);
@@ -256,7 +245,6 @@ app.post('/all_domains/delete', checkAuth, function (req, res) {
 
 app.get('/all_domains/new', function (req, res) {
     var url=req.query.url;
-    console.log("req: %s", url);
     
     connection.query('CALL insert_domain(?, ?);', [url, req.session.user_id], function(err, docs) {
         if (err) res.json(err);
@@ -272,85 +260,60 @@ app.get('/all_domains/new', function (req, res) {
                 
             res.end(body);
         }
-        //res.redirect('my_domains');
     })
 });
 
 app.post('/all_domains/new', function (req, res) {
-    //var domain=req.body.domain;
     var domain = req.query.url;
-
-
-    //console.log("domain: %s", domain);
-    console.log("domain: %s", domain);
-    console.log("body: %j", req.body);
-    //console.log("req: %j", req);
-    //console.log(JSON.stringify(req.body));
+    if (typeof domain === "undefined" || domain == '') {
+        res.send('');
+        return;
+    }
 
     var links = req.body.hrefs;
-    var user = req.body.user;
-    //var links_result;
+    var secretUsername = req.body.user;
+    connection.query("SELECT user FROM users WHERE secret_username = ?", [secretUsername], function(err, usernameDocs) {
+        if(err) throw err;
 
-    if (typeof domain === "undefined") {
-        res.send('');
-        return;
-    }
+        if(usernameDocs.length == 1) {
+           var user = usernameDocs[0].user;
 
-    if(domain == '') {
-        res.send('');
-        return;
-    }
+           connection.query('CALL insert_domain(?, ?);', [domain, user], function(err, InsertDomainDocs) {
+                if(err) throw err;
+                connection.query('CALL get_links(?, ?);', [domain, user], function(err, getLinksDocs, fields) {
+                    if(err) throw err;
+                    var responseArray = getLinksDocs[0];
+                    var responseArrayLen = responseArray.length;
+                    var responseObject = {};
 
-    connection.query('CALL insert_domain(?, ?);', [domain, user], function(err0, docs0) {
-        //if (err) res.json(err);
-    
-        connection.query('CALL get_links(?, ?);', [domain, user], function(err, docs, fields) {
-            //if (err) res.json(err);
+                    for (var i = 0; i < responseArrayLen; i++) {
+                        if (i == 0) {
+                            responseObject.rate = responseArray[i].rate;
+                            responseObject.bc_rate = responseArray[i].bc_rate;
+                        }
 
-            //row = [domain | link | bc_link | user_link | rate | bc_rate]
+                        var key = responseArray[i].link;
+                        responseObject[key] = { bc_link: responseArray[i].bc_link, user_link: responseArray[i].user_link }
+                    }
 
-            var responseArray = docs[0];
+                    res.writeHead(200, {
+                        'Content-Type': 'application/json',
+                        'Access-Control-Allow-Origin': '*',
+                        'Access-Control-Allow-Methods': 'POST, OPTIONS',
+                        'Access-Control-Allow-Headers': 'X-Requested-With, Content-Type, Origin, Accept' 
+                    });
 
-            var responseArrayLen = responseArray.length;
-            var responseObject = {};
+                    res.end(JSON.stringify(responseObject));
 
-            for (i = 0; i < responseArrayLen; i++) {
-                if (i == 0) {
-                    responseObject.rate = responseArray[i].rate;
-                    responseObject.bc_rate = responseArray[i].bc_rate;
-                }
-
-                var key = responseArray[i].link;
-                responseObject[key] = { bc_link: responseArray[i].bc_link, user_link: responseArray[i].user_link }
-            }
-
-            res.writeHead(200, {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Methods': 'POST, OPTIONS',
-                'Access-Control-Allow-Headers': 'X-Requested-With, Content-Type, Origin, Accept' 
+                    for (var key in links) {
+                        connection.query('CALL insert_link(?, ?, ?);', [domain, key, user], function(err2, rows) {
+                        //if (err) res.json(err);
+                        });
+                    }
+                }); 
             });
-
-    //            res.send({
-    //                    result: 'success',
-    //                    err:    '',
-    //                    fields: docs,
-    //                    json:   responseObject
-    //                    length: rows.length
-    //            });
-
-            res.end(JSON.stringify(responseObject));
-
-            for (var key in links) {
-                connection.query('CALL insert_link(?, ?, ?);', [domain, key, user], function(err2, rows) {
-                //if (err) res.json(err);
-                });
-            }
-
-        }); 
-
+        }
     });
-
 });
 
 app.post('/all_domains/edit_rate', checkAuth, function (req, res) {
@@ -361,12 +324,11 @@ app.post('/all_domains/edit_rate', checkAuth, function (req, res) {
     });
 });
 
-/******************** links ********************/
 
 app.get('/edit_form', checkAuth, function (req, res) {
     var domain=req.query.domain;
     connection.query('CALL get_links(?, ?)', [domain, req.session.user_id], function(err, docs) {
-        console.log(docs);
+
         if(docs[0][0]) {
             res.render('edit_form', {rows: docs[0]}); 
         }  
@@ -384,7 +346,6 @@ app.get('/links_fail_page', checkAuth, function (req, res) {
 
 app.get('/links_admin', checkAdmin, function (req, res) {
     var domain=req.query.domain;
-    console.log("req: %s", domain);
     connection.query('SELECT * FROM links WHERE domain LIKE CONCAT('%', ?, '%')', [domain], function(err, docs) {
         res.render('links_admin', {rows: docs});
     });
@@ -399,17 +360,36 @@ app.post('/links/edit', checkAuth, function (req, res) {
         user_link = "http://" + user_link;
     }
 
-    console.log("req: %s", domain);
     connection.query('CALL insert_user_link(?, ?, ?, ?);', [domain, link, user_link, req.session.user_id], function(err, docs) {
-        //connection.query('CALL get_links(?, ?)', [domain, req.session.user_id], function(err, docs2) {
-        //        res.render('edit_form', {rows: docs2[0]}); 
-        //});
         res.redirect('edit_form?domain=' + domain);
     });
 });
 
-//start server
 
+//Get and load client js
+app.get('/jquery', function (req, res) {
+    var user=req.query.version;
+    connection.query("SELECT secret_username FROM users WHERE secret_username = ?", [user], function(err, docs) {
+        if(docs.length == 1) {            
+            fs.readFile('./client/compressed/compressed-landercode.js', function(err, data) {
+                if(err) throw err;
+                
+                var replacedFile = String(data).replace('replaceme', user);       
+               
+                res.writeHead(200, {
+                        'Content-Length': replacedFile.length,
+                        'Content-Type': 'text/plain',
+                        'Access-Control-Allow-Origin': '*',
+                        'Access-Control-Allow-Methods': 'GET, OPTIONS',
+                        'Access-Control-Allow-Headers': 'X-Requested-With, Content-Type, Origin, Accept' 
+                    });
+                    res.end(replacedFile);
+
+
+            }); 
+        }
+    });
+});
 
 module.exports = app;
 
