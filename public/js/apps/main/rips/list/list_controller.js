4ee34dd698be21970e3d81abdcf8244cf74d1b0e
Buildcave <trevor@buildcave.com>
2015-04-02 22:58:56 -0700

    changing dist folder name to recommit it so it fucking works no reason why this should be like this

--- public/js/apps/main/rips/list/list_controller.js
+++ public/js/apps/main/rips/list/list_controller.js
@@ -4,8 +4,8 @@ define(["app", "apps/main/rips/list/list_view"], function(RipManager, RipsListVi
       listRips: function(criterion){
         require(["apps/main/rips/list/list_model",
                  "apps/main/offers/list/list_model",
-                 "common/loading_view"], function(getRipsModel, getOffersModel, LoadingView){
-
+                 "common/loading_view",
+                 "apps/main/rips/edit/edit_view"], function(getRipsModel, getOffersModel, LoadingView, EditRipView){
           var ripsListLayout = new RipsListView.RipsListLayout();
           ripsListLayout.render();
 
@@ -15,67 +15,67 @@ define(["app", "apps/main/rips/list/list_view"], function(RipManager, RipsListVi
           ripsListLayout.ripsTableRegion.show(loadingView);
 
           var fetchingRips = RipManager.request("rips:getrips");
+          var fetchingOffers = RipManager.request("offers:getoffers");
           $.when(fetchingRips).done(function(rips){
-            var fetchingOffers = RipManager.request("offers:getoffers");
-            $.when(fetchingOffers).done(function(offers){
-              var ripsListView = new RipsListView.Rips({
+            var ripsListView = new RipsListView.Rips({
                 collection: rips
-              });
+            });
 
-              var saveRipSuccess = function(model, message, other) {
-                ripsListView.trigger("rip:edit:notify", message.success, "success");
-              };
+            var saveRipSuccess = function(model, message, other) {
+              ripsListView.trigger("rip:edit:notify", message.success, "success");
+            };
 
-              var saveRipError = function(model, message, other) {
-                ripsListView.trigger("rip:edit:notify", message.error, "danger");
-                model.set(model.previousAttributes());
-              };
+            var saveRipError = function(model, message, other) {
+              ripsListView.trigger("rip:edit:notify", message.error, "danger");
+              model.set(model.previousAttributes());
+            };
 
+            
+         
+
+            $.when(fetchingOffers).done(function(offers){
               //first check if main application has loaded, must load that first
               //it sets up some main things for the main app including left nav
               //the main layout, etc. TODO
-              require(["apps/main/rips/edit/edit_view"], function(EditRipView){
-                ripsListView.on("childview:rip:edit", function(viewTestTodo, args){
+             
+              ripsListView.on("childview:rip:edit", function(viewTestTodo, args){
 
-                  var model = args.model;
+                var model = args.model;
 
-                  //TODO pass offer list to view as well but don't let it be
-                  //part of the model so on save it doesnt do weird shit
+                //TODO pass offer list to view as well but don't let it be
+                //part of the model so on save it doesnt do weird shit
 
-                  var view = new EditRipView.Rip({
-                    model: model,
-                    offerList: offers
-                  });
-
-                  view.on("rip:edit:submit", function(data){
-                    
-                    if(this.model.isValid(true)) {
-                      model.save(data, {success: saveRipSuccess, error: saveRipError});
-                      view.closeDialog();
-                    } else {
-                      //TODO This doesn't contain the actual previous attr right now
-                      //because of the validation (i think)
-                      view.model.set(view.model.previousAttributes());
-                    }
-                    
-                  });
-
-
-                  view.on("close", function(){
-                    args.view.trigger("remove:highlightrow");
-                  });
-
-                  view.showDialog();
-
+                var view = new EditRipView.Rip({
+                  model: model,
+                  offerList: offers
                 });
-              });
-              
 
-              try {
-                ripsListLayout.ripsTableRegion.show(ripsListView);
-              } catch(e){}
-              
+                view.on("rip:edit:submit", function(data){
+                  
+                  if(this.model.isValid(true)) {
+                    model.save(data, {success: saveRipSuccess, error: saveRipError});
+                    view.closeDialog();
+                  } else {
+                    //TODO This doesn't contain the actual previous attr right now
+                    //because of the validation (i think)
+                    view.model.set(view.model.previousAttributes());
+                  }
+                  
+                });
+
+
+                view.on("close", function(){
+                  args.view.trigger("remove:highlightrow");
+                });
+
+                view.showDialog();
+              });
             });
+           
+            try {
+              ripsListLayout.ripsTableRegion.show(ripsListView);
+            } catch(e){}
+            
           });
         });
       }
