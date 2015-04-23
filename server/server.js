cf5576a88ced4b28d44593627d8e087e3110ce46
troy <troyjaeger@gmail.com>
2015-04-23 13:09:42 -0700

    added hourly stats to url_report_for_n_days

--- server/server.js
+++ server/server.js
@@ -512,12 +512,16 @@ app.get('/url_report_for_n_days', checkAuth, function (req, res) {
     var days = req.query.n;
     var id = req.query.id;
     var user = req.signedCookies.user_id;
-    var jacks_list;
-    var hits_list;
 
-    var hits_ret_arr = [];
-    var jacks_ret_arr = [];
-    var ret_obj = [];
+    var daily_jacks_list;
+    var daily_hits_list;
+    var daily_hits_ret_arr = [];
+    var daily_jacks_ret_arr = [];
+    var daily_ret_obj = [];
+
+    var hourly_jacks_list;
+    var hourly_hits_list;
+    var hourly_ret_obj = [];
 
     var db_query = '';
     if(req.signedCookies.admin == 'true') {
@@ -535,37 +539,83 @@ app.get('/url_report_for_n_days', checkAuth, function (req, res) {
                 console.log(docs[0]);
 
                 if(docs[0][0]) {
-                    hits_list = docs[0][0].hits_list;
-                    jacks_list = docs[0][0].jacks_list;
+                    daily_hits_list = docs[0][0].hits_list;
+                    daily_jacks_list = docs[0][0].jacks_list;
 
-                    var hits_arr = hits_list.split(',');
-                    hits_arr = hits_arr.reverse();
+                    var daily_hits_arr = daily_hits_list.split(',');
+                    daily_hits_arr = daily_hits_arr.reverse();
                     
-                    var jacks_arr = jacks_list.split(',');
-                    jacks_arr = jacks_arr.reverse();
+                    var daily_jacks_arr = daily_jacks_list.split(',');
+                    daily_jacks_arr = daily_jacks_arr.reverse();
 
                     for (var j = 0; j < days; j++) {
-                        if(!jacks_ret_arr[j]) {
-                            jacks_ret_arr[j] = 0;
+                        if(!daily_jacks_ret_arr[j]) {
+                            daily_jacks_ret_arr[j] = 0;
                         }
-                        if(jacks_arr[j]) {
-                            jacks_ret_arr[j] += Number(jacks_arr[j]);
+                        if(daily_jacks_arr[j]) {
+                            daily_jacks_ret_arr[j] += Number(daily_jacks_arr[j]);
                         }
 
-                        if(!hits_ret_arr[j]) {
-                            hits_ret_arr[j] = 0;
+                        if(!daily_hits_ret_arr[j]) {
+                            daily_hits_ret_arr[j] = 0;
                         }
-                        if(hits_arr[j]) {
-                            hits_ret_arr[j] += Number(hits_arr[j]);
+                        if(daily_hits_arr[j]) {
+                            daily_hits_ret_arr[j] += Number(daily_hits_arr[j]);
                         }
                     };
                     
                     for (var i = 0; i < days; i++) {
                         var d = moment().subtract(i + 1, 'days').format('YYYY-MM-DD');
-                        ret_obj[i] = {day:d, hits:hits_ret_arr[i], jacks:jacks_ret_arr[i]};
+                        daily_ret_obj[i] = {day:d, hits:daily_hits_ret_arr[i], jacks:daily_jacks_ret_arr[i]};
                     };
-                    res.status(200);
-                    res.json(ret_obj);
+
+                    db.query('CALL get_hourly_stats(?, ?);', [id, user], function(err2, docs2) {
+                        if (err2) {
+                            console.log(err2);
+                            res.status(500);
+                            res.json({error: "Internal server error looking up the hourly stats for id = " + id +"."});
+                        } else {       
+
+                            if(docs2[0][0]) {
+
+                                var hourly_hits_list = docs2[0][0].hits_list;
+                                if(docs2[0][0].hits) {
+                                    //add current hour hits
+                                    hourly_hits_list = hourly_hits_list + ',' + docs2[0][0].hits;
+                                }
+
+                                var hourly_jacks_list = docs2[0][0].jacks_list;
+                                if(docs2[0][0].jacks) {
+                                    //add current hour jacks
+                                    hourly_jacks_list = hourly_jacks_list + ',' + docs2[0][0].jacks; 
+                                }
+
+                                if(hourly_hits_list.charAt(0) == ',') {
+                                    hourly_hits_list = hourly_hits_list.substring(1, hourly_hits_list.length); //remove preceding ',' if it's there
+                                }
+                                if(hourly_jacks_list.charAt(0) == ',') {
+                                    hourly_jacks_list = hourly_jacks_list.substring(1, hourly_jacks_list.length); //remove preceding ',' if it's there
+                                }
+
+                                var hourly_hits_arr = hourly_hits_list.split(',');
+                                var hourly_jacks_arr = hourly_jacks_list.split(',');
+
+                                var curr_hour = moment().hour();
+                                for (var i = 0; i < curr_hour; i++) {
+                                    var day_hour = moment().utc().hour(i).startOf('hour');
+                                    hourly_ret_obj[i] = {hour:day_hour, hits:Number(hourly_hits_arr[i]), jacks:Number(hourly_jacks_arr[i])}; 
+                                };
+
+                                res.status(200);
+                                res.json({daily_stats:daily_ret_obj, hourly_stats:hourly_ret_obj});
+                            }
+                            else {
+                                res.status(500);
+                                res.json({error: "Internal server error looking up the hourly stats for id = " + id +"."});
+                            }
+                        }
+                    });
+
                 }
                 else {
                     res.status(500);
